6618856edc9a7060365fd9fe8c0d63e6513a8dd9
DKBTM <62892662+DKBTM@users.noreply.github.com>
2020-04-22 12:06:12 +0200

    Added couple of tasks

--- script.js
+++ script.js
@@ -396,7 +396,8 @@ const f27 = () => {
 document.getElementById("t27cal").addEventListener("wheel", f27)
 
 // 28. Write a JavaScript program to check whether two given integer values are 
-// in the range 50..99 (inclusive). Return true if either of them are in the said range.
+// in the range 50..99 (inclusive). Return true if either of them are in 
+// the said range.
 
 const f28 = () => {
     no1 = document.getElementById("t28a").value;
@@ -428,3 +429,381 @@ const f29 = () => {
 }
 
 document.getElementById("t29cal").addEventListener("click", f29)
+
+// 30. Write a JavaScript program to check whether a string "Script" presents 
+// at 5th (index 4) position in a given string, if "Script" presents in the 
+// string return the string without "Script" otherwise return the original one.
+
+const f30 = () => {
+    const core = "Script";
+    const string = document.getElementById("t30a").value;
+    const check = string.search(core);
+    if (check == 4) {
+        document.getElementById("t30").innerHTML= string.slice(0,4)+string.slice(10);
+    } else {
+        document.getElementById("t30").innerHTML=string;
+    }
+}
+
+document.getElementById("t30cal").addEventListener("click", f30)
+
+// 31. Write a JavaScript program to find the largest of three given integers.
+
+const f31 = () => {
+    let integers = {
+        a: Number(document.getElementById("t31a").value),
+        b: Number(document.getElementById("t31b").value),
+        c: Number(document.getElementById("t31c").value)
+};
+    if (integers.a>integers.b && integers.a>integers.c) {
+        document.getElementById("t31").innerHTML=`The largest of given integers is the integer ${integers.a}.`
+    } else if (integers.b>integers.a && integers.b>integers.c) {
+        document.getElementById("t31").innerHTML=`The largest of given integers is the integer ${integers.b}.`
+    } else if (integers.c>integers.a && integers.c>integers.b) {
+        document.getElementById("t31").innerHTML=`The largest of given integers is the integer ${integers.c}.`
+    } else {
+        document.getElementById("t31").innerHTML=`There is no largest integer here!`
+    }
+} 
+
+document.getElementById("t31cal").addEventListener("click", f31)
+
+// 32. Write a JavaScript program to find a value which is 
+// nearest to 100 from two different given integer values.
+
+const f32 = () => {
+    const no1=Number(document.getElementById("t32a").value);
+    const no2=Number(document.getElementById("t32b").value);
+    if (Math.abs(100-no1)<Math.abs(100-no2)) {
+        document.getElementById("t32").innerHTML=`The nearest no. is ${no1}.`
+    } else if (Math.abs(100-no1)>Math.abs(100-no2)) {
+        document.getElementById("t32").innerHTML=`The nearest no. is ${no2}.`
+    } else {
+        document.getElementById("t32").innerHTML=`Can't assess the nearest no.`
+    }
+}
+
+document.getElementById("t32cal").addEventListener("click", f32)
+
+// 33. Write a JavaScript program to check whether two numbers are 
+// in range 40..60 or in the range 70..100 inclusive.
+
+const f33 = () => {
+    const no1=Number(document.getElementById("t33a").value);
+    const no2=Number(document.getElementById("t33b").value);
+    if ((no1>=40&&no1<=60 || no1>=70&&no1<=100) && 
+    ((no2>=40&&no2<=60 || no2>=70&&no2<=100))) {
+        document.getElementById("t33").innerHTML=`Both integers are within the range!`
+    } else {
+        document.getElementById("t33").innerHTML=`The condition is not met.`
+    }
+} 
+
+document.getElementById("t33cal").addEventListener("click", f33)
+
+// 34. Write a JavaScript program to find the larger number from the two 
+// given positive integers, the two numbers are in the range 40..60 inclusive.
+
+const f34 = () => {
+    const no1=Number(document.getElementById("t34a").value);
+    const no2=Number(document.getElementById("t34b").value);
+    if (no1>=40 && no1<=60 && no2>=40 && no2<=60) {
+        document.getElementById("t34").innerHTML = `The bigger no. is ${Math.max(no1, no2)}!`
+    } else {
+        document.getElementById("t34").innerHTML = `Can't assess the bigger no.`
+    }
+}
+
+document.getElementById("t34cal").addEventListener("click", f34)
+
+// 35. Write a program to check whether a specified character 
+// exists within the 2nd to 4th position in a given string.
+
+const f35 = () => {
+    const word=document.getElementById("t35a").value;
+    const char=document.getElementById("t35b").value;
+    if (word.search(char)>0 && word.search(char)<4) {
+        document.getElementById("t35").innerHTML = `Yes, the character is there.`
+    } else {
+        document.getElementById("t35").innerHTML = `No, the character isn't there.`
+    }
+}
+
+document.getElementById("t35cal").addEventListener("click", f35)
+
+// 36. Write a JavaScript program to check whether the last digit 
+// of the three given positive integers is same.
+
+const f36 = () => {
+    const no1 = document.getElementById("t36a").value;
+    const no2 = document.getElementById("t36b").value;
+    const no3 = document.getElementById("t36c").value;
+
+    const arr1 = no1.split("");
+    const arr2 = no2.split("");
+    const arr3 = no3.split("");
+
+    const last1 = arr1[arr1.length-1];
+    const last2 = arr2[arr2.length-1];
+    const last3 = arr3[arr3.length-1];
+
+    if (last1==last2 && last1 == last3) {
+        document.getElementById("t36").innerHTML = `Yes, the last digit of all 3 integers is the same`;
+    } else {
+        document.getElementById("t36").innerHTML = `No, not all digits are the same.`
+    }
+}
+
+document.getElementById("t36cal").addEventListener("click", f36)
+
+// 37. Write a JavaScript program to create new string with first 3 characters 
+// are in lower case from a given string. If the string length is less than 3 
+// convert all the characters in upper case.
+
+const f37 = () => {
+    const word = document.getElementById("t37a").value;
+    const three = word.substring(0,3).toLowerCase();
+    const rest = word.substring(3);
+    if (word.length<4) {
+        document.getElementById("t37").innerHTML = `The new word is ${three.toUpperCase()+rest}`
+    } else {document.getElementById("t37").innerHTML = `The new word is ${three+rest}`
+    }
+}
+
+document.getElementById("t37cal").addEventListener("click", f37)
+
+// 38. Write a JavaScript program to check the total marks of a student 
+// in various examinations. The student will get A+ grade if the total 
+// marks are in the range 89..100 inclusive, if the examination is "Final-exam." 
+// the student will get A+ grade and total marks must be greater than 
+// or equal to 90. Return true if the student get A+ grade or false otherwise.
+
+// 39. Write a JavaScript program to compute the sum of the two given integers, 
+// If the sum is in the range 50..80 return 65 other wise return 80.
+
+const f39 = () => {
+    const no1=Number(document.getElementById("t39a").value);
+    const no2=Number(document.getElementById("t39b").value);
+    const sum = no1 + no2;
+    if (sum>=50 && sum<=80) {
+        document.getElementById("t39").innerHTML = `65`
+    } else {
+        document.getElementById("t39").innerHTML = `80`
+    }
+}
+
+document.getElementById("t39cal").addEventListener("click", f39)
+
+// 40. Write a JavaScript program to check from two given integers whether 
+// one of them is 8 or their sum or difference is 8.
+
+const f40 = () => {
+    const no1=Number(document.getElementById("t40a").value);
+    const no2=Number(document.getElementById("t40b").value);
+    if (no1==8 || no2==8 || no1-no2==8 || no1+no2 == 8) {
+        document.getElementById("t40").innerHTML = `Yeah, there's an 8 in one of numbers or sum/difference`
+    } else {
+        document.getElementById("t40").innerHTML = `Nope`
+    }
+}
+
+document.getElementById("t40cal").addEventListener("click", f40)
+
+// 41. Write a JavaScript program to check three given numbers, 
+// if the three numbers are same return 30 otherwise return 20 
+// and if two numbers are same return 40.
+
+const f41 = () => {
+    const no1=Number(document.getElementById("t41a").value);
+    const no2=Number(document.getElementById("t41b").value);
+    const no3=Number(document.getElementById("t41c").value);
+
+    if (no1 == no2 && no1 == no3) {
+        document.getElementById("t41").innerHTML = `30`
+    } else if ((no1==no2 && no1!=no3) || (no1==no3 && no1!=no2) || (no2==no3 && no1!=no3)) {
+        document.getElementById("t41").innerHTML = `40`
+    } else {
+        document.getElementById("t41").innerHTML = `20`
+    }
+}
+
+document.getElementById("t41cal").addEventListener("click", f41)
+
+// 42. Write a JavaScript program to check whether three given numbers are 
+// increasing in strict mode or in soft mode.
+// Note: Strict mode -> 10, 15, 31 : Soft mode -> 24, 22, 31 or 22, 22, 31
+
+const f42 = () => {
+    const no1=Number(document.getElementById("t42a").value);
+    const no2=Number(document.getElementById("t42b").value);
+    const no3=Number(document.getElementById("t42c").value);
+
+    if (no2>no1 && no3>no2) {
+        document.getElementById("t42").innerHTML = `Numbers are increasing in strict mode`
+    } else if (no2>no1 || no3>no2) {
+        document.getElementById("t42").innerHTML = `Numbers are increasing in soft mode`
+    } else {
+        document.getElementById("t42").innerHTML = `Numbers are not increasing`
+    }
+}
+
+document.getElementById("t42cal").addEventListener("click", f42)
+
+// 43. Write a JavaScript program to check from three given numbers 
+// (non negative integers) that two or all of them have the same rightmost digit.
+
+const f43 = () => {
+    const no1=document.getElementById("t43a").value;
+    const no2=document.getElementById("t43b").value;
+    const no3=document.getElementById("t43c").value;
+
+    const arr1 = no1.split("");
+    const arr2 = no2.split("");
+    const arr3 = no3.split("");
+
+    const last1 = arr1[arr1.length-1];
+    const last2 = arr2[arr2.length-1];
+    const last3 = arr3[arr3.length-1];
+
+    if (last1==last2 || last1==last3 || last2==last3) {
+        document.getElementById("t43").innerHTML = `Yes`
+    } else {
+        document.getElementById("t43").innerHTML = `No`
+    }
+}
+
+document.getElementById("t43cal").addEventListener("click", f43)
+
+// Ignore: 44-47
+
+// 48. Write a JavaScript program to reverse a given string.
+
+const f48 = () => {
+    const word = document.getElementById("t48a").value;
+    const arr = word.split("");
+    const newArr = arr.reverse();
+    const newWord = newArr.join("");
+    document.getElementById("t48").innerHTML=newWord
+}
+
+document.getElementById("t48cal").addEventListener("click", f48)
+
+// 49. Write a JavaScript program to replace every character in 
+// a given string with the character following it in the alphabet.
+
+const f49 = () => {
+    const word = document.getElementById("t49a").value;
+    const arr=word.split("");
+    let fill = [];
+    const uni=arr.forEach( (letter) => {
+        fill.push(letter.charCodeAt()+1)
+    })
+    let newArr = [];
+    const uni2 = fill.forEach ((letter) => {
+        newArr.push(String.fromCharCode(letter))
+    })
+    const newWord = newArr.join("");
+    document.getElementById("t49").innerHTML=newWord;
+}
+
+document.getElementById("t49cal").addEventListener("click", f49)
+
+// 50. Write a JavaScript program to capitalize the first letter of 
+// each word of a given string.
+
+const f50 = () => {
+    const word = document.getElementById("t50a").value;
+    const arr = word.split(" ");
+    let sent = [];
+    const arr2 = arr.forEach((i) => {
+        sent.push(i[0].toUpperCase()+i.substr(1));
+    })
+    document.getElementById("t50").innerHTML=sent.join(" ");
+}
+
+document.getElementById("t50cal").addEventListener("click", f50)
+
+// Ignore: 51-54
+
+// 55. Write a JavaScript program to check whether a given string 
+// contains equal number of p's and t's.
+
+const f55 = () => {
+    const word = document.getElementById("t55a").value.toLowerCase();
+    const arr = word.split("");
+    let ps = [];
+    let ts = [];
+    const arr2 = arr.forEach((i) => {
+        if (i=="p") {
+            ps.push(1);
+        } else if (i=="t") {
+            ts.push(1);
+        }
+    })
+    if (ps.length == ts.length) {
+        document.getElementById("t55").innerHTML=`Yes`
+    } else {
+        document.getElementById("t55").innerHTML=`No`
+    }
+}
+
+document.getElementById("t55cal").addEventListener("click", f55)
+
+// Ignore: 56-84
+
+// 85. Write a JavaScript code to divide a given array of positive integers 
+// into two parts. First element goes to first part, second element 
+// goes to second part, and third element goes to first part and so on. 
+// Now compute the sum of two parts and store into an array of size two.
+
+const f85 = () => {
+    const no1=document.getElementById("t85a").value;
+    const arr1=no1.split("");
+    let even=[];
+    let odd = [];
+    const arr2 = arr1.forEach((i) => {
+        let j = arr1.indexOf(i)+1;
+        if (j%2!=0) {
+            odd.push(Number(i));
+        } else if (j%2==0) {
+            even.push(Number(i));
+        }
+    })
+    let result1 = even.reduce((acc,num) => {
+        return acc + num
+    }, 0);
+    let result2 = odd.reduce((acc,num) => {
+        return acc + num
+    }, 0);
+    document.getElementById("t85").innerHTML = `Sum of even digits is ${result1} and sum of odd digits is ${result2}.`
+}
+
+document.getElementById("t85cal").addEventListener("click", f85)
+
+// Ignore: 86 - 90
+
+// 91. Write a JavaScript program to find the maximum possible sum of 
+// some of its two consecutive numbers (numbers that follow each other in order.) 
+// of a given array of positive integers.
+
+const f91 = () => {
+    const no1=document.getElementById("t91a").value;
+    const arr = no1.split("");
+    const sumArray = arr.map(num => {
+        return Number(num)
+    })
+
+    const results = []
+    let checkArray = sumArray.reduce((a,b) => {
+        results.push(a+b);
+        return b;
+    })
+
+    let highest = results.reduce((a,b) => {
+        return Math.max(a,b);
+    })
+    document.getElementById("t91").innerHTML=highest;
+}
+
+document.getElementById("t91cal").addEventListener("click", f91)
+
